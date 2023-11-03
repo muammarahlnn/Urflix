26e3a516cb7b9e9df1bcd4c56d59f84f4dace603
muammarahlnn <21muammarahlan@gmail.com>
2023-11-04 06:07:50 +0800

    feat(app): add splash screen

--- app/src/main/java/com/muammarahlnn/urflix/MainActivity.kt
+++ app/src/main/java/com/muammarahlnn/urflix/MainActivity.kt
@@ -3,14 +3,18 @@ package com.muammarahlnn.urflix
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.muammarahlnn.urflix.core.designsystem.theme.UrflixTheme
 import com.muammarahlnn.urflix.ui.UrflixApp
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContent {
             UrflixTheme {
                 UrflixApp()
