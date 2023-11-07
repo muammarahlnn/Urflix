6e52f4e41e38a9fe6046a65476e4a9bb188fba5b
muammarahlnn <21muammarahlan@gmail.com>
2023-11-07 17:43:23 +0800

    fix(network): fix TvShowDetailsResponse's runtime

--- core/network/src/main/java/com/muammarahlnn/urflix/core/network/model/response/TvShowDetailsResponse.kt
+++ core/network/src/main/java/com/muammarahlnn/urflix/core/network/model/response/TvShowDetailsResponse.kt
@@ -14,9 +14,21 @@ data class TvShowDetailsResponse(
     val name: String,
     @SerialName("first_air_date") val firstAirDate: String? = null,
     @SerialName("vote_average") val voteAverage: Float? = null,
-    val runtime: Int? = null,
     val overview: String? = null,
     @SerialName("backdrop_path") val backdropPath: String? = null,
     @SerialName("poster_path") val posterPath: String? = null,
     val genres: List<GenreResponse>? = null,
+    @SerialName("last_episode_to_air") val lastEpisodeToAir: EpisodeResponse? = null,
+)
+
+@Serializable
+data class EpisodeResponse(
+    val id: Int,
+    val name: String? = null,
+    val overview: String? = null,
+    @SerialName("vote_average") val voteAverage: Float? = null,
+    @SerialName("air_date") val airDate: String? = null,
+    @SerialName("episode_number") val episodeNumber: Int? = null,
+    val runtime: Int? = null,
+    @SerialName("season_number") val seasonNumber: Int? = null,
 )
